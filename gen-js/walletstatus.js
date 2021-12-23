c720bb1dee1a776e930cfcebde84ba2c050a8bd1
congruity7 <congruity7@gmail.com>
2021-12-23 15:22:53 +0530

    Added wallet status

---
+++ gen-js/walletstatus.js
@@ -0,0 +1,24 @@
+// source: proto/moonshot.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.v1.WalletStatus');
+
+/**
+ * @enum {number}
+ */
+proto.v1.WalletStatus = {
+  WALLET_STATUS_UNDEFINED: 0,
+  WALLET_STATUS_CONNECTED: 1,
+  WALLET_STATUS_DISCONNECTED: 2
+};
+

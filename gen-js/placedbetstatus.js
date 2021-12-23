209e28c346d50ca621d9654dc0301ac857c3674b
congruity7 <congruity7@gmail.com>
2021-12-23 15:11:20 +0530

    Added auto gen files

---
+++ gen-js/placedbetstatus.js
@@ -0,0 +1,25 @@
+// source: proto/moonshot.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.v1.PlacedBetStatus');
+
+/**
+ * @enum {number}
+ */
+proto.v1.PlacedBetStatus = {
+  PLACED_BET_STATUS_UNDEFINED: 0,
+  PLACED_BET_STATUS_PENDING: 1,
+  PLACED_BET_STATUS_PAID_OUT: 3,
+  PLACED_BET_STATUS_BUSTED: 4
+};
+
